89791d48669e43d2813c410b05e2912578055783
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2024-12-25 11:47:56 +0000

    initial commit

---
+++ app/users/data-table.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DataTable() {
+  return (
+    <div>data-table</div>
+  )
+}
+
+export default DataTable
---
+++ app/users/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <section className="py-24">
+        <div className="container">
+            <h1 className="text-3x1 font-bold">All Users</h1>
+        </div>
+    </section>
+  )
+}
+
+export default Page
